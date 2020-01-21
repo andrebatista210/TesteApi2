94400032490b0d8ce8fed6a325148dd074ed87ce
GRUPORCP\aluiz <aluiz@tasken.com>
2020-01-21 09:26:10 -0300

    Adicionar arquivos de projeto.

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TesteApi2.Model;
+
+namespace TesteApi2.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options) : base(options){}
+        public DbSet<Evento> Eventos { get; set; }
+
+    }
+}
---
+++ Model/Evento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteApi2.Model
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QdtPessoas { get; set; }
+        public string Lote { get; set; }
+        public string ImagemURL { get; set; }
+    }
+}
